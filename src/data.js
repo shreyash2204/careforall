f238797686b23619b20d0d022953aa1de2f56d30
Rahul Kumar <130215217+rahul-1105@users.noreply.github.com>
2024-05-23 01:13:36 +0530

    chore: Update image sources in data.js

--- src/data.js
+++ src/data.js
@@ -1,107 +1,113 @@
+import card1 from "./assets/card-1.jpg";
+import card2 from "./assets/card-2.jpg";
+import card3 from "./assets/card-3.jpg";
+import card4 from "./assets/card-4.jpg";
+import card5 from "./assets/card-5.jpg";
+
 export const items = [
-    {
-      title: "SEND CHILDREN BACK TO SCHOOL",
-      desc: "If not now they might never return",
-      img: "./src/assets/card-1.jpg",
-    },
-    {
-      title: "SUPPORT KITCHEN GARDENS",
-      desc: "Give children sustainable nutrition",
-      img: "./src/assets/card-2.jpg",
-    },
-    {
-      title: "BRIDGE THE EDUCATION GAP",
-      desc: "Through academic support centres",
-      img: "./src/assets/card-3.jpg",
-    },
-    {
-      title: "STOP CHILD LABOUR",
-      desc: "Help children go to school instead",
-      img: "./src/assets/card-4.jpg",
-    },
-    {
-      title: "HELP UNDERPRIVILEGED MOTHERS",
-      desc: "Provide them the nutritional care",
-      img: "./src/assets/card-5.jpg",
-    },
-  ];
+  {
+    title: "SEND CHILDREN BACK TO SCHOOL",
+    desc: "If not now they might never return",
+    img: `${card1}`,
+  },
+  {
+    title: "SUPPORT KITCHEN GARDENS",
+    desc: "Give children sustainable nutrition",
+    img: `${card2}`,
+  },
+  {
+    title: "BRIDGE THE EDUCATION GAP",
+    desc: "Through academic support centres",
+    img: `${card3}`,
+  },
+  {
+    title: "STOP CHILD LABOUR",
+    desc: "Help children go to school instead",
+    img: `${card4}`,
+  },
+  {
+    title: "HELP UNDERPRIVILEGED MOTHERS",
+    desc: "Provide them the nutritional care",
+    img: `${card5}`,
+  },
+];
 
 export const stories = [
-    {
-      title: "From A School Dropout To Becoming A Google Woman Icon",
-      desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
-      img: "./src/assets/card-1.jpg",
-      bgColor: "bg-red-500",
-    },
-    {
-      title: "From A School Dropout To Becoming A Google Woman Icon",
-      desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
-      img: "./src/assets/card-1.jpg",
-      bgColor: "bg-pink-500",
-    },
-    {
-      title: "From A School Dropout To Becoming A Google Woman Icon",
-      desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
-      img: "./src/assets/card-1.jpg",
-      bgColor: "bg-blue-500",
-    },
-    {
-      title: "From A School Dropout To Becoming A Google Woman Icon",
-      desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
-      img: "./src/assets/card-1.jpg",
-      bgColor: "bg-green-500",
-    },
-    
-  ];
+  {
+    title: "From A School Dropout To Becoming A Google Woman Icon",
+    desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
+    img: "./src/assets/card-1.jpg",
+    bgColor: "bg-red-500",
+  },
+  {
+    title: "From A School Dropout To Becoming A Google Woman Icon",
+    desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
+    img: "./src/assets/card-1.jpg",
+    bgColor: "bg-pink-500",
+  },
+  {
+    title: "From A School Dropout To Becoming A Google Woman Icon",
+    desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
+    img: "./src/assets/card-1.jpg",
+    bgColor: "bg-blue-500",
+  },
+  {
+    title: "From A School Dropout To Becoming A Google Woman Icon",
+    desc: "Mamta Gujjar fought against all odds to continue her education and become a footballer! Inspired by her story of struggle, Google selected her for its #SearchForChange campaign.",
+    img: "./src/assets/card-1.jpg",
+    bgColor: "bg-green-500",
+  },
+];
 
-  export const whatWeDo = [
-    {
-      border: "border-pink-500",
-      title: "Education",
-      img:"/src/assets/issue-1.gif"
-    },
-    {
-      border: "border-teal-500",
-      title: "Health & Nutrition",
-      img:"/src/assets/issue-2.gif"
-    },
-    {
-      border: "border-purple-500",
-      title: "Saftey & Shelter",
-      img:"/src/assets/issue-3.gif"
-    },
-    {
-      border: "border-orange-500",
-      title: "Child Participation",
-      img:"/src/assets/issue-4.gif"
-    }
-  ]
+export const whatWeDo = [
+  {
+    border: "border-pink-500",
+    title: "Education",
+    img: "/src/assets/issue-1.gif",
+  },
+  {
+    border: "border-teal-500",
+    title: "Health & Nutrition",
+    img: "/src/assets/issue-2.gif",
+  },
+  {
+    border: "border-purple-500",
+    title: "Saftey & Shelter",
+    img: "/src/assets/issue-3.gif",
+  },
+  {
+    border: "border-orange-500",
+    title: "Child Participation",
+    img: "/src/assets/issue-4.gif",
+  },
+];
 
-  export const stats = [
-    {
-      numbers: "21,13,617",
-      desc: "children impacted overall",
-      subDesc: "",
-    },
-    {
-      numbers: "94%",
-      desc: "children in CFA project areas, between the ages of 6-18 years, in school",
-      subDesc: "*In the 15-18 age group, our enrolment and retention rate is 13% better than the all India enrolment status – GER. (based on the UDISE+ 2021-22 report)",
-    },
-    {
-      numbers: "92%",
-      desc: "children in CFA project areas, under the age of 5 years, protected from undernourishment",
-      subDesc: "*Only 8% children in CFA project areas are underweight, as compared to 32% all India status as per NFHS 5",
-    },
-    {
-      numbers: "99%",
-      desc: "of adolescent girls, in CFA project areas, between 11-18 years, protected from child marriage",
-      subDesc: "",
-    },
-    {
-      numbers: "88%",
-      desc: "children, in CFA project areas, between the ages of 6-18 years, protected from child labour",
-      subDesc: "",
-    },
-  ];
-  
+export const stats = [
+  {
+    numbers: "21,13,617",
+    desc: "children impacted overall",
+    subDesc: "",
+  },
+  {
+    numbers: "94%",
+    desc: "children in CFA project areas, between the ages of 6-18 years, in school",
+    subDesc:
+      "*In the 15-18 age group, our enrolment and retention rate is 13% better than the all India enrolment status – GER. (based on the UDISE+ 2021-22 report)",
+  },
+  {
+    numbers: "92%",
+    desc: "children in CFA project areas, under the age of 5 years, protected from undernourishment",
+    subDesc:
+      "*Only 8% children in CFA project areas are underweight, as compared to 32% all India status as per NFHS 5",
+  },
+  {
+    numbers: "99%",
+    desc: "of adolescent girls, in CFA project areas, between 11-18 years, protected from child marriage",
+    subDesc: "",
+  },
+  {
+    numbers: "88%",
+    desc: "children, in CFA project areas, between the ages of 6-18 years, protected from child labour",
+    subDesc: "",
+  },
+];
