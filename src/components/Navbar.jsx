aa5e8918c04402bb132838e59a9025c25accced1
Rahul Kumar <130215217+rahul-1105@users.noreply.github.com>
2024-05-23 00:38:25 +0530

    feat: Update logo source in Navbar component
    The logo source in the Navbar component was updated to "../assets/logo.png" to fix the broken image link.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+im;
 
 const Navbar = () => {
   return (
     <header className="fixed top-0 w-full bg-white z-50">
       <div className="md:w-[80%] lg:w-[70%] h-[80px] mx-auto px-4 flex justify-between">
         <NavLink to="/">
-          <img src="/src/assets/logo.png" alt="logo" className="h-[100px] " />
+          <img src="../assets/logo.png" alt="logo" className="h-[100px] " />
         </NavLink>
         <ul className="flex h-full items-center gap-8">
           <li>
